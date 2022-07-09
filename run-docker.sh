3eba4f9108056d08f9d5ed2796e26010afbe86b0
qiushaocloud <qiushaocloud@126.com>
2022-07-09 09:23:39 +0800

    update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -8,7 +8,7 @@ fi
 GITLAB_VOLUMES_RUNNER_CONFIG=`grep GITLAB_VOLUMES_RUNNER_CONFIG $CURR_DIR/.env|grep -v grep|awk -F '=' '{print $2}' | sed 's/ //g' | sed "s/'//g" | sed 's/\"//g'`
 if [ ! -f "$GITLAB_VOLUMES_RUNNER_CONFIG/config.toml" ];then
   echo "file $GITLAB_VOLUMES_RUNNER_CONFIG/config.toml is not exist, copy $CURR_DIR/test-volumes/runner-config/config.toml to $GITLAB_VOLUMES_RUNNER_CONFIG/config.toml"
-  mkdir $GITLAB_VOLUMES_RUNNER_CONFIG
+  mkdir -p $GITLAB_VOLUMES_RUNNER_CONFIG
   copy $CURR_DIR/test-volumes/runner-config/config.toml $GITLAB_VOLUMES_RUNNER_CONFIG/config.toml
 fi
 
